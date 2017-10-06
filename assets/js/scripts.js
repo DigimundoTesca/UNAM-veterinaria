1110cf33b84304537a7ad03e29ee10d711a09a45
Itzli <itzli2000@msn.com>
2017-10-06 12:28:04 -0500

    Testing showing sweet alert and then send question form

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,32 @@
 $(document).ready(function() {
-  console.log('oliwis');
-  
+  $("#mailitbtn").on("click", function(e) {
+      e.preventDefault(); //This is to Avoid Page Refresh and Fire the Event "Click"
+      let form = $('#mailit');
+      swal({
+        title: "Su pregunta será enviada!",
+        text: "Espere pronto una respuesta",
+        type: "success",
+        showCancelButton: false,
+        confirmButtonText: "Entendido",
+        confirmButtonClass: 'btn btn-success'
+      }).then(function () {
+          // sendmail();
+        });
+    });
+
+  // function sendmail() {
+  //   $("form.container").submit(function(e) {
+  //     e.preventDefault();
+  //     var data = $(this).serialize();
+  //     var url = $(this).attr('action');
+  //     var form = $(this);
+  //     $.post(url,data,function(data){
+  //       $(.result).html(data.result+"Watchlist");
+  //     });
+  //     return false;
+  //   });
+  // }
+
+
 });
 
