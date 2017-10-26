74430fc99eb5cbb851c5c59c70e5086c7edb1e3d
Itzli <itzli2000@msn.com>
2017-10-26 11:18:58 -0500

    Testing image upload from question form

--- attach.php
+++ attach.php
@@ -1,140 +1,95 @@
 <?php
-$to      = $_POST['itzli2000@msn.com']; 
-$email   = $_POST['itzli2000@msn.com']; 
-$name    = $_POST['animal'];
-$subject = $_POST['Estado_fisiologico']; 
-$comment = $_POST['pregLarga'];
+if(isset($_POST['submit']))
+{
+  $encoding = "utf-8";
+// Preferences for Subject field
+  $subject_preferences = array(
+    "input-charset" => $encoding,
+    "output-charset" => $encoding,
+    "line-length" => 76,
+    "line-break-chars" => "\r\n"
+  );
 
-$To          = strip_tags($to);
-$TextMessage =strip_tags(nl2br($comment),"<br>");
-$HTMLMessage =nl2br($comment);
-$FromName    =strip_tags($name);
-$FromEmail   =strip_tags($email);
-$Subject     =strip_tags($subject);
+// Multiple recipients
+  $recipients = array(
+    'ceo@digimundo.com.mx',
+    'frontend@digimundo.com.mx',
+    'itzli2000@msn.com',
+  );
+  // $to = implode(',', $recipients);
+  $from_mailto = $_POST['itzli2000@msn.com'];
+  $patient_animal = $_POST['animal'];
+  $patient_weight = $_POST['Peso'];
+  $patient_gender = $_POST['Sexo'];
+  $patient_physiological = $_POST['Estado_fisiologico'];
+  $patient_fc = $_POST['FC'];
+  $patient_fr = $_POST['FR'];
+  $patient_temp = $_POST['Temp'];
+  $patient_ruminal = $_POST['MovRum'];
+  $patient_clinical = $_POST['Clinica'];
+  $patient_quickQ = $_POST['pregunta'];
+  $patient_question = $_POST['pregLarga'];
 
-$boundary1   =rand(0,9)."-"
-.rand(10000000000,9999999999)."-"
-.rand(10000000000,9999999999)."=:"
-.rand(10000,99999);
-$boundary2   =rand(0,9)."-".rand(10000000000,9999999999)."-"
-.rand(10000000000,9999999999)."=:"
-.rand(10000,99999);
+// Subject
+  $subject = 'Se ha creado una nueva pregunta';
 
+// Message
+  $message = '
+  <html>
+  <head>
+  <title>Nueva pregunta.</title>
+  </head>
+  <body>
+  <h2>'.$patient_quickQ.'</h2>
+  <p> Mensaje: <br>'. $patient_question .'</p>
+  <h4>Datos del animal:</h4>
+  <p>Espécimen: '.$patient_animal.'<br>Peso: '.$patient_weight.' <br> Sexo: '.$patient_gender.' <br>Estado fisiológico: '.$patient_physiological.' <br>Frecuencia cardiaca: '. $patient_fc .' <br>Frecuencia respiratoria: '. $patient_fr .' <br>Temperatura: '. $patient_temp .' <br>Movimientos ruminales: '. $patient_ruminal .' <br>Historia clínica: '. $patient_clinical .' </p>
+  </body>
+  </html>
+  ';
+        //The form has been submitted, prep a nice thank you message
+  $output = '<h1>Thanks for your file and message!</h1>';
 
-for($i=0; $i < count($_FILES['images']['name']); $i++){
-if(is_uploaded_file($_FILES['fileatt']['tmp_name'][$i]) && 
-   !empty($_FILES['fileatt']['size'][$i]) && 
-   !empty($_FILES['fileatt']['name'][$i])){
-     
-$attach      ='yes';
-$end         ='';
+        //Deal with the email
+  $to = 'itzli2000@msn.com';
+  $attachment = chunk_split(base64_encode(file_get_contents($_FILES['file']['tmp_name'])));
+  $filename = $_FILES['file']['name'];
 
-   $handle      =fopen($_FILES['fileatt']['tmp_name'][$i], 'rb'); 
-   $f_contents  =fread($handle, $_FILES['fileatt']['size'][$i]); 
-   $attachment[]=chunk_split(base64_encode($f_contents));
-   fclose($handle); 
+  $boundary =md5(date('r', time())); 
 
-$ftype[]       =$_FILES['fileatt']['type'][$i];
-$fname[]       =$_FILES['fileatt']['name'][$i];
+  // To send HTML mail, the Content-type header must be set
+  // Mail header
+  $header = "From: Pet Gurú\r\nReply-To: softwaremanager@digimundo.com";
+  $header .= "\r\nMIME-Version: 1.0\r\nContent-Type: multipart/mixed; charset=".$encoding." boundary=\"_1_$boundary\"";
+  $header .= "MIME-Version: 1.0 \r\n";
+  $header .= "Content-Transfer-Encoding: 8bit \r\n";
+  $header .= "Fecha: ".date("r (T)")." \r\n";
+  $header .= iconv_mime_encode("Asunto", $patient_quickQ, $subject_preferences);
+
+  $message="This is a multi-part message in MIME format.
+
+  --_1_$boundary
+  Content-Type: multipart/alternative; boundary=\"_2_$boundary\"
+
+  --_2_$boundary
+  Content-Type: text/plain; charset=\"iso-8859-1\"
+  Content-Transfer-Encoding: 7bit
+
+  $message
+
+  --_2_$boundary--
+  --_1_$boundary
+  Content-Type: application/octet-stream; name=\"$filename\" 
+  Content-Transfer-Encoding: base64 
+  Content-Disposition: attachment 
+
+  $attachment
+  --_1_$boundary--";
+
+  // Mail it
+  mail($to, $subject, $message, $header);
+
+  // Redirect
+  header('Location: foro.html');
 }
-}
-
-/***************************************************************
-Creating Email: Headers, BODY
-1- HTML Email WIthout Attachment!! <<-------- H T M L ---------
-***************************************************************/
-#---->Headers Part
-$Headers     =<<<AKAM
-From: $FromName <$FromEmail>
-Reply-To: $FromEmail
-MIME-Version: 1.0
-Content-Type: multipart/alternative;
-    boundary="$boundary1"
-AKAM;
-
-#---->BODY Part
-$Body        =<<<AKAM
-MIME-Version: 1.0
-Content-Type: multipart/alternative;
-    boundary="$boundary1"
-
-This is a multi-part message in MIME format.
-
---$boundary1
-Content-Type: text/plain;
-    charset="windows-1256"
-Content-Transfer-Encoding: quoted-printable
-
-$TextMessage
---$boundary1
-Content-Type: text/html;
-    charset="windows-1256"
-Content-Transfer-Encoding: quoted-printable
-
-$HTMLMessage
-
---$boundary1--
-AKAM;
-
-/***************************************************************
-2- HTML Email WIth Multiple Attachment <<----- Attachment ------
-***************************************************************/
-
-if($attach=='yes') {
-
-$attachments='';
-$Headers     =<<<AKAM
-From: $FromName <$FromEmail>
-Reply-To: $FromEmail
-MIME-Version: 1.0
-Content-Type: multipart/mixed;
-    boundary="$boundary1"
-AKAM;
-
-for($j=0;$j<count($ftype); $j++){
-$attachments.=<<<ATTA
---$boundary1
-Content-Type: $ftype[$j];
-    name="$fname[$i]"
-Content-Transfer-Encoding: base64
-Content-Disposition: attachment;
-    filename="$fname[$j]"
-
-$attachment[$j]
-
-ATTA;
-}
-
-$Body        =<<<AKAM
-This is a multi-part message in MIME format.
-
---$boundary1
-Content-Type: multipart/alternative;
-    boundary="$boundary2"
-
---$boundary2
-Content-Type: text/plain;
-    charset="windows-1256"
-Content-Transfer-Encoding: quoted-printable
-
-$TextMessage
---$boundary2
-Content-Type: text/html;
-    charset="windows-1256"
-Content-Transfer-Encoding: quoted-printable
-
-$HTMLMessage
-
---$boundary2--
-
-$attachments
---$boundary1--
-AKAM;
-}
-
-/***************************************************************
-Sending Email
-***************************************************************/
-$ok=mail($To, $Subject, $Body, $Headers);
-echo $ok?"<h1> Mail Sent</h1>":"<h1> Mail not SEND</h1>";
 ?>
